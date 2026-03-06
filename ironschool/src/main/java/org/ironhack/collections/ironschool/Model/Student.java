e074c1ecd81baa63967a2625eb0385052916906b
Shebnem.M <snmmrdv@gmail.com>
2026-03-06 17:11:00 +0400

    feat: implement Student class with auto-ID

--- ironschool/src/main/java/org/ironhack/collections/ironschool/Model/Student.java
+++ ironschool/src/main/java/org/ironhack/collections/ironschool/Model/Student.java
@@ -1,4 +1,51 @@
 package org.ironhack.collections.ironschool.Model;
 
 public class Student {
+    private String studentId;
+    private String name;
+    private String address;
+    private String email;
+    private Course course;
+    public Student(String name, String address, String email){
+        this.studentId=java.util.UUID.randomUUID().toString();
+        this.name=name;
+        this.address=address;
+        this.email=email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public String getStudentId() {
+        return studentId;
+    }
+
+    public Course getCourse() {
+        return course;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public void setCourse(Course course) {
+        this.course = course;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 }
