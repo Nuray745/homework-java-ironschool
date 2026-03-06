f8028b57fbfd093a832f3d9d83363ea87e27febb
Shebnem.M <snmmrdv@gmail.com>
2026-03-06 16:47:03 +0400

    feat: implement Teacher class with auto-ID generator

--- ironschool/src/main/java/org/ironhack/collections/ironschool/Model/Teacher.java
+++ ironschool/src/main/java/org/ironhack/collections/ironschool/Model/Teacher.java
@@ -1,4 +1,34 @@
 package org.ironhack.collections.ironschool.Model;
 
+import java.util.UUID;
+
 public class Teacher {
+      private String teacherId;
+      private String name;
+      private double salary;
+      public Teacher(String name, double salary){
+          this.teacherId= UUID.randomUUID().toString();
+          this.name=name;
+          this.salary=salary;
+      }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public String getTeacherId() {
+        return teacherId;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
 }
