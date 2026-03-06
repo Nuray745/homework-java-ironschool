0b684ecba8af9c1a4819a526a478f8ce90a52e9e
Nuray Taghiyeva <tagiyevanuray35@gmail.com>
2026-03-06 14:58:18 +0400

    new file added

---
+++ ironschool/src/test/java/org/ironhack/collections/ironschool/IronschoolApplicationTests.java
@@ -0,0 +1,13 @@
+package org.ironhack.collections.ironschool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IronschoolApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
