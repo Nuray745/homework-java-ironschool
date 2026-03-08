c1fa6f15bf1b3e544173e109bbee710a31a71f66
Shebnem.M <snmmrdv@gmail.com>
2026-03-08 13:29:05 +0400

    feat:implement student enrollment logic and lookup Id

--- ironschool/src/main/java/org/ironhack/collections/ironschool/Controller/StudentController.java
+++ ironschool/src/main/java/org/ironhack/collections/ironschool/Controller/StudentController.java
@@ -1,14 +1,63 @@
 package org.ironhack.collections.ironschool.Controller;
 
+import org.ironhack.collections.ironschool.Model.Course;
 import org.ironhack.collections.ironschool.Model.Student;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.ironhack.collections.ironschool.Service.CourseService;
+import org.ironhack.collections.ironschool.Service.StudentService;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
 @RequestMapping("/api/students")
 public class StudentController {
+   private final StudentService studentService;
+   private final CourseService courseService;
+   public StudentController(StudentService studentService, CourseService courseService){
+       this.studentService=studentService;
+       this.courseService=courseService;
+   }
+   @GetMapping
+   public List<Student> displayAllStudents(){
+       List<Student> students=studentService.getAllStudent();
+       if(students==null || students.isEmpty()){
+           System.out.println("No students found in the system");
+       }
+       return students;
+   }
+   @GetMapping("/{id}")
+   public Student lookupStudent(@PathVariable String id){
+      try{
+         Student student=studentService.getStudentById(id);
+         if(student!=null){
+             return student;
+         }
+         else{
+             System.out.println("Student with ID " + id + " not found");
+             return null;
+         }
+      }
+      catch(Exception e){
+          System.out.println("Unexpected error happened: "+e.getMessage());
+          return null;
+       }
+   }
+
+   @PostMapping("/enroll")
+    public String enrollStudent(@RequestParam String id, @RequestParam String courseId) {
+       try {
+           String response = courseService.enrollStudent(courseId);
+           if (!response.equals("Course ID not found")) {
+               Student s = studentService.getStudentById(id);
+               if (s != null) {
+                   s.setCourse(courseService.getCourseById(courseId));
+               }
+               return "Success: " + s.getName() + " enrolled in " + courseId;
+           }
+           return response;
+       } catch (Exception e) {
+           return "Unexpected error happened " + e.getMessage();
+       }
+   }
 
 }
